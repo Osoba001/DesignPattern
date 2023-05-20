54c26cc78528e07fa6cb9ba9a010abadf0d3286c
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-05-20 21:06:11 +0100

    Added Composite Design pattern

---
+++ CompositePattern/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    public class Employee:IEmployee
+    {
+        public Employee(string name, string dept)
+        {
+            Name = name;
+            Dept = dept;
+        }
+
+        public string Name { get; }
+        public string Dept { get; }
+
+        public void GetDetails()
+        {
+            Console.WriteLine($"Name : {Name}, Dept : {Dept}");
+        }
+    }
+}
---
+++ CompositePattern/Manager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    internal class Manager : IEmployee
+    {
+        public List<Employee> Subordinate;
+        public Manager(string name, string dept)
+        {
+            Name = name;
+            Dept = dept;
+            Subordinate =new List<Employee>();
+        }
+
+        public string Name { get; }
+        public string Dept { get; }
+
+        public void GetDetails()
+        {
+            Console.WriteLine($"Name : {Name}, Dept : {Dept}");
+            foreach (var emp in Subordinate)
+            {
+                emp.GetDetails();
+            }
+        }
+    }
+}
